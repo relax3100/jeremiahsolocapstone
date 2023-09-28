5cddc1b5e765e6a6daeb7b43411c36c83fc1958d
Jer w <jeremiahwwinters@gmail.com>
2023-09-27 19:37:43 -0600

    second push i think

--- cookiePage.ts
+++ cookiePage.ts
@@ -4,15 +4,29 @@ import {BasePage} from "./basePage"
 export class CookieClicker extends BasePage {
     engBtn: By = By.css("#langSelect-EN")
     cookieBtn: By = By.css("#bigCookie")
-    cursorU: By = By.xpath('(//div[@class="product unlocked enabled"])[1]');
+    cursorU: By = By.xpath('(//div[@class="product unlocked enabled"])[1]')
     grandmaU: By = By.xpath('(//div[@class="product unlocked enabled"])[2]')
+    farmU: By = By.xpath('(//div[@class="product unlocked enabled"])[3]')
     legacyBtn: By = By.css("#legacyButton")
     ascendBtn: By = By.xpath('//a[text()="Ascend"]')
     upgrade1: By = By.css('#upgrade0')
     achievmentX: By = By.xpath('//div[@class="framed close sidenote"]')
     statsBtn: By = By.css('#statsButton')
     gotIt: By = By.xpath('//a[text()="Got it!"]')
-    
+    mute1: By = By.xpath('//div[@id="productMute1"]')
+    mute2: By = By.xpath('//div[@id="productMute2"]')
+    unmute1: By = By.xpath('//div[@id="mutedProduct1"]')
+    unmute2: By = By.xpath('//div[@id="mutedProduct2"]')
+    options: By = By.xpath('//div[@id="prefsButton"]')
+    infoBtn: By = By.xpath('//div[@id="logButton"]')
+    reincarnBtn: By = By.xpath('//a[@id="ascendButton"]')
+    reincarnYes: By = By.xpath('//a[@id="promptOption0"]')
+    closeMenu: By = By.xpath('//div[@class="close menuClose"]')
+    twitterBtn: By = By.css('#topbarTwitter');
+    sellBtn: By = By.xpath('//div[@id="storeBulkSell"]')
+    sellMax: By = By.xpath('//div[@id="storeBulkMax"]')
+    tumblrBtn: By = By.xpath('//a[@id="topbarTumblr"]')
+
 
 constructor() {
     super({url:"https://orteil.dashnet.org/cookieclicker/"})
@@ -31,4 +45,5 @@ async tabSwitch() {
     await this.driver.close();
     await this.driver.switchTo().window(myTabs[0]);
 };
-}
+
+};
--- cookieTest.test.ts
+++ cookieTest.test.ts
@@ -1,40 +1,76 @@
 import { CookieClicker } from "./cookiePage";
 const cookie = new CookieClicker
+const fs = require("fs")
 
-test('Can click the cookie, and upgrade', async () => {
+test('Testing english button, upgrades, buying, selling, twitter, tumblr, legacy upgrading, and all of the other mechanics are working as intended.', async () => {
     await cookie.navigate();
     await cookie.driver.manage().window().maximize();
     await cookie.click(cookie.engBtn);
     await cookie.driver.sleep(3000)
     await cookie.click(cookie.gotIt);
-    await cookie.repeatClick(20, cookie.cookieBtn)
-    await cookie.click(cookie.cursorU)
-    await cookie.repeatClick(100, cookie.cookieBtn)
-    await cookie.click(cookie.upgrade1)
-    await cookie.repeatClick(106, cookie.cookieBtn)
-    await cookie.repeatClick(2, cookie.grandmaU)
-    await cookie.repeatClick(50, cookie.cookieBtn)
-    await cookie.repeatClick(5, cookie.cursorU)
-    await cookie.repeatClick(35, cookie.cookieBtn)
-    await cookie.repeatClick(2, cookie.cursorU)
-    await cookie.repeatClick(240, cookie.cookieBtn)
-    await cookie.click(cookie.upgrade1)
-    await cookie.repeatClick(35, cookie.cookieBtn)
-    await cookie.click(cookie.grandmaU)
-    await cookie.repeatClick(35, cookie.cookieBtn)
-    await cookie.click(cookie.grandmaU)
-    await cookie.repeatClick(35, cookie.cookieBtn)
-    await cookie.click(cookie.grandmaU)
-    await cookie.repeatClick(200, cookie.cookieBtn)
-    await cookie.repeatClick(5, cookie.cursorU)
-    await cookie.repeatClick(155, cookie.cookieBtn)
-    await cookie.click(cookie.upgrade1)
-    await cookie.repeatClick(155, cookie.cookieBtn)
-    await cookie.repeatClick(5, cookie.grandmaU)
-    await cookie.repeatClick(55, cookie.cookieBtn)
-    await cookie.repeatClick(5, cookie.grandmaU)
-    await cookie.repeatClick(55, cookie.cookieBtn)
-    await cookie.click(cookie.grandmaU)
-    
+    await cookie.repeatClick(20, cookie.cookieBtn);
+    await cookie.click(cookie.cursorU);
+    await cookie.repeatClick(100, cookie.cookieBtn);
+    await cookie.click(cookie.upgrade1);
+    await cookie.repeatClick(106, cookie.cookieBtn);
+    await cookie.repeatClick(2, cookie.grandmaU);
+    await cookie.repeatClick(50, cookie.cookieBtn);
+    await cookie.repeatClick(5, cookie.cursorU);
+    await cookie.repeatClick(35, cookie.cookieBtn);
+    await cookie.repeatClick(2, cookie.cursorU);
+    await cookie.repeatClick(240, cookie.cookieBtn);
+    await cookie.click(cookie.upgrade1);
+    await cookie.repeatClick(35, cookie.cookieBtn);
+    await cookie.click(cookie.grandmaU);
+    await cookie.repeatClick(35, cookie.cookieBtn);
+    await cookie.click(cookie.grandmaU);
+    await cookie.repeatClick(35, cookie.cookieBtn);
+    await cookie.click(cookie.grandmaU);
+    await cookie.repeatClick(200, cookie.cookieBtn);
+    await cookie.repeatClick(5, cookie.cursorU);
+    await cookie.repeatClick(155, cookie.cookieBtn);
+    await cookie.click(cookie.upgrade1);
+    await cookie.repeatClick(250, cookie.cookieBtn);
+    await cookie.click(cookie.farmU);
+    await cookie.click(cookie.mute1);
+    await cookie.click(cookie.mute2);
+    await cookie.click(cookie.unmute1);
+    await cookie.click(cookie.unmute2);
+    await cookie.click(cookie.mute1);
+    await cookie.click(cookie.mute2);
+    await cookie.click(cookie.unmute1);
+    await cookie.click(cookie.unmute2);
+    await cookie.click(cookie.mute1);
+    await cookie.click(cookie.mute2);
+    await cookie.click(cookie.unmute1);
+    await cookie.click(cookie.unmute2);
+    await cookie.click(cookie.achievmentX);
+    await cookie.click(cookie.options);
+    await cookie.click(cookie.statsBtn);
+    await cookie.click(cookie.infoBtn);
+    await cookie.click(cookie.sellBtn);
+    await cookie.click(cookie.sellMax);
+    await cookie.click(cookie.cursorU);
+    await cookie.click(cookie.grandmaU);
+    await cookie.click(cookie.legacyBtn);
+    await cookie.click(cookie.ascendBtn);
+    await cookie.click(cookie.reincarnBtn);
+    await cookie.click(cookie.reincarnYes);
+    await cookie.click(cookie.closeMenu);
+    await cookie.click(cookie.twitterBtn);
+    await cookie.tabSwitch();
+    await cookie.click(cookie.tumblrBtn);
+    await cookie.tabSwitch();
+    await cookie.driver.sleep(3000)
+    await fs.writeFile(`${__dirname}/cookieClicker.png`,
+    await cookie.driver.takeScreenshot(), "base64",
+    (e) => {
+        if (e) console.error(e)
+        else console.log('Save Succesful')
+    }
+    )
+    await cookie.driver.quit();
 
+
+    
 });
